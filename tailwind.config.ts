a78707bcf71e2c566d5b93dfdb4bcff91750889d
Mete_Work_Mac <mthngns78@gmail.com>
2024-11-05 15:34:32 +0300

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,32 @@
 import type { Config } from "tailwindcss";
-
+const colors = require("tailwindcss/colors");
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      eterationBlue: "#2A59FE",
+      eterationBlueActive: "#3b65fe",
+      eterationSmoke: "#F9F9F9",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+      emerald: colors.emerald,
+      indigo: colors.indigo,
+      yellow: colors.yellow,
+      red: colors.red,
     },
   },
   plugins: [],
