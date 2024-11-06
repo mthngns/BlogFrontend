7719a87babb5db90931361ce302b337cba620ded
Mete_Work_Mac <mthngns78@gmail.com>
2024-11-06 11:33:08 +0300

    dummy: some dummy changes

--- src/app/blogList/page.tsx
+++ src/app/blogList/page.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+const roboto = Red_Hat_Display({subsets:["latin"]});
+
+import { Montserrat, Red_Hat_Display, Roboto, Roboto_Mono } from 'next/font/google'
+ 
+// const roboto = Roboto({
+//   weight: '400',
+//   subsets: ['latin'],
+//   display: 'swap',
+// })
 
 const BlogList = () => {
+  console.log('çalıştım')
+
   return (
-    <div>BlogList</div>
+    <div className={roboto.className+' font-semibold'} >blogfront.</div>
   )
 }
 
