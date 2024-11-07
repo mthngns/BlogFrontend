c7af37771ac0a3fb97fd58edb954a6b3db482949
Mete_Work_Mac <mthngns78@gmail.com>
2024-11-07 15:23:39 +0300

    fix:theme mod icons fixed.

--- src/app/features/Header/components/ThemeSwitcher.tsx
+++ src/app/features/Header/components/ThemeSwitcher.tsx
@@ -10,8 +10,8 @@ const ThemeSwitcher = () => {
     const { theme, setTheme } = useTheme()
   return (
     <div className={'flex justify-center items-center text-xs gap-x-2 min-w-8 rounded-md cursor-pointer'}>
-      {theme ==="dark" && <MdLightMode className='text-orange-300' size={20} onClick={() => setTheme('light')}/>}
-      {theme ==="light" && <FaMoon className='text-sky-100' size={16} onClick={() => setTheme('dark')}/>}
+      {theme ==="light" && <MdLightMode className='text-orange-300 h-full w-full p-1' size={16} onClick={() => setTheme('dark')}/>}
+      {theme ==="dark" && <FaMoon className='text-sky-100 h-full w-full p-2' size={16} onClick={() => setTheme('light')}/>}
     </div>
   )
 }
